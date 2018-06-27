74bb9de167c81d346615dda1b411cb96dd288e05
Vicki Hamm <vicki.cello@gmail.com>
2018-06-27 18:10:09 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -61,9 +61,7 @@ attr_reader :species
   end
 
   def list_pets
-    "I have #{pets[:fishes].count} fish,
-    #{pets[:dogs].count} dog(s), and
-    #{pets[:cats].count} cat(s)."
+    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
   end
 
 end
