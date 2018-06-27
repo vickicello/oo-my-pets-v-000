aacd42c2cf3f84980efc56a7ae3094a9ea95ca7c
Vicki Hamm <vicki.cello@gmail.com>
2018-06-27 18:03:16 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -25,5 +25,20 @@ attr_reader :species
     @@all = []
   end
 
+  def say_species
+    "I am a human."
+  end
+
+  def buy_fish(name)
+    @pets[:fishes] << Fish.new(name)
+  end
+
+  def buy_cat(name)
+    @pets[:cats] << Cat.new(name)
+  end
+
+  def buy_dog(name)
+    @pets[:dogs] << Dog.new(name)
+  end
 
 end
