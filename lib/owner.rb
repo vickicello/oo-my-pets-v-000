2d4e5afaf72ddcac6f62190f4a8784ed49b655cc
Vicki Hamm <vicki.cello@gmail.com>
2018-06-27 17:48:47 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,24 @@
 class Owner
-  # code goes here
+
+attr_reader :species
+
+  @@all = []
+  @@owner_counter = 0
+
+  def initialize(name, species, pets)
+    @name = name
+    @species = species
+    @pets = {}
+    @@owner_counter += 1
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@owner_counter = 0
+  end
+
+
 end
