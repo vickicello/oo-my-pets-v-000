03c66bc1bb77f28c901a912bdfc9d24abe8a0eb1
Vicki Hamm <vicki.cello@gmail.com>
2018-06-27 17:53:05 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -8,18 +8,18 @@ attr_reader :species
   def initialize(name)
     @name = name
     @species = species
-    @pets = {}
+    @pets = {fishes: [], cats: [], dogs: []}
     @@owner_counter = 0
   end
 
   def self.all
     @@all
   end
-  
+
   def save
      @@all << self
    end
-   
+
   def self.reset_all
     @@owner_counter = 0
   end
